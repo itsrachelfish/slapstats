d41970126aeb703959989305e6babce3e2cdf37b
Rachel Fish <itsrachelfish@gmail.com>
2015-01-28 23:58:14 -0700

    Updated slapcounter to split file into lines xD

--- slapcounter.js
+++ slapcounter.js
@@ -1,7 +1,14 @@
 var fs = require('fs');
 
-fs.readFile('./slaplog.txt', {encoding: 'ascii'}, function(err, data)
+fs.readFile('./slaplog.txt', {encoding: 'ascii'}, function(error, data)
 {
-    if (err) throw err;
-    console.log(data);
+    if (error) throw error;
+
+    // Split data into individual lines
+    var events = data.toString().split("\n");
+
+    for(var i = 0, l = events.length; i < l; i++)
+    {
+        console.log(events[i]);
+    }
 });
