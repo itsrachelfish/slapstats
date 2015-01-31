93d3e6dc300047c8c06de592f3c0bd2bd5fe9b5c
Rachel Fish <itsrachelfish@gmail.com>
2015-01-31 00:30:55 -0700

    lol EADDRINUSE

--- slapserver.js
+++ slapserver.js
@@ -2,7 +2,7 @@ var express =   require('express'),
     app     =   express(),
     server  =   require('http').Server(app);
 
-server.listen(1337);
+server.listen(7331);
 console.log("Slap Stats Server Started!~");
 
 app.use(express.static(__dirname + '/static'));
